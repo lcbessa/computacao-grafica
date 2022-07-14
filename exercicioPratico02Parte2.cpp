542396dae83cb385ffeb16a9571cc6c247c47ef2
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-07-13 21:02:27 -0300

    Atividade 2 - Parte 2: Quarto com ZBuffer e BackFaceCulling

---
+++ exercicioPratico02Parte2.cpp
@@ -0,0 +1,133 @@
+#include <GL/glut.h>
+
+#include <iostream>
+
+GLfloat horzangle = -45.0, vertangle = 30.0;
+int var = 1;
+int zBuffer = 1;
+int backFace = -1;
+
+void RenderScene(void) {
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -3.0f);
+    glRotatef(vertangle, 1.0f, 0.0f, 0.0f);
+    glRotatef(horzangle, 0.0f, 1.0f, 0.0f);
+
+    if (backFace == 1) {
+        glEnable(GL_CULL_FACE);
+    } else {
+        glDisable(GL_CULL_FACE);
+    }
+    if (zBuffer == 1) {
+        glEnable(GL_DEPTH_TEST);
+    } else {
+        glDisable(GL_DEPTH_TEST);
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_LINES);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.7f, 0.0f, 0.0f);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.7f);
+    glEnd();
+    glBegin(GL_POLYGON);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.2f, 0.0f);
+    glVertex3f(0.2f, 0.3f, 0.0f);
+    glVertex3f(0.4f, 0.2f, 0.0f);
+    glVertex3f(0.4f, 0.0f, 0.0f);
+    glEnd();
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.0f, 0.4f);
+    glVertex3f(0.0f, 0.2f, 0.4f);
+    glVertex3f(0.0f, 0.2f, 0.0f);
+    glEnd();
+    glBegin(GL_QUAD_STRIP);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, 0.2f, 0.0f);
+    glVertex3f(0.0f, 0.2f, 0.4f);
+    glVertex3f(0.2f, 0.3f, 0.0f);
+    glVertex3f(0.2f, 0.3f, 0.4f);
+    glVertex3f(0.4f, 0.2f, 0.0f);
+    glVertex3f(0.4f, 0.2f, 0.4f);
+    glEnd();
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_LINE_STRIP);
+    glVertex3f(0.0f, 0.0f, 0.4f);
+    glVertex3f(0.4f, 0.0f, 0.4f);
+    glVertex3f(0.4f, 0.0f, 0.0f);
+    glEnd();
+    glBegin(GL_LINES);
+    glVertex3f(0.4f, 0.0f, 0.4f);
+    glVertex3f(0.4f, 0.2f, 0.4f);
+    glEnd();
+    glFlush();
+    glutSwapBuffers();
+}
+
+void ChangeSize(GLsizei width, GLsizei height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if (var == 1) {
+        gluPerspective(30.0, 1, 1, 10);
+    } else {
+        glOrtho(-0.7, 0.7, -0.7, 0.7, 1.0, 10.0);
+    }
+}
+
+void keyboardFunc(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'p':
+            var *= -1;
+            ChangeSize(500, 500);
+            break;
+        case 'w':
+            vertangle -= 5;
+            break;
+        case 's':
+            vertangle += 5;
+            break;
+        case 'a':
+            horzangle -= 5;
+            break;
+        case 'd':
+            horzangle += 5;
+            break;
+        case 'z':
+            zBuffer *= -1;
+            break;
+        case 'b':
+            backFace *= -1;
+            break;
+    }
+    glutPostRedisplay();
+}
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Exercicio-02 - Parte 2");
+    glutReshapeFunc(ChangeSize);
+    glutKeyboardFunc(keyboardFunc);
+    glutDisplayFunc(RenderScene);
+    // Escrever informacoes sobre a versao de OpenGL em uso porque pode ser util saber.
+    std::cout << "Usando OpenGL '" << glGetString(GL_VERSION) << "' implementado por '"
+              << glGetString(GL_VENDOR) << "' em arquitetura '" << glGetString(GL_RENDERER)
+              << "'.\nExtensoes disponiveis: " << glGetString(GL_EXTENSIONS) << std::endl;
+
+    glutMainLoop();
+    return 0;
+}
